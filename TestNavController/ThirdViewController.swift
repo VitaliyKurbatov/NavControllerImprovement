8d4d3a8dba4d07a5523bc717e3e2bcd47a38d4e7
Vitaliy Kurbatov <kurbatovvitaliy@yandex.ru>
2021-08-10 17:27:20 +0300

    Add headerView for tableView sections

--- TestNavController/ThirdViewController.swift
+++ TestNavController/ThirdViewController.swift
@@ -13,6 +13,7 @@ class ThirdViewController: UIViewController {
         tv.translatesAutoresizingMaskIntoConstraints = false
         tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tv.dataSource = self
+        tv.delegate = self
         return tv
     }()
     
@@ -20,6 +21,8 @@ class ThirdViewController: UIViewController {
         super.loadView()
         setupView()
     }
+    
+    let array = [[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,15 +54,53 @@ class ThirdViewController: UIViewController {
 }
 
 
-extension ThirdViewController: UITableViewDataSource {
+extension ThirdViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return array.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return array[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = String(indexPath.row + 1)
+        cell.textLabel?.text = String(array[indexPath.section][indexPath.row]) // "\(array[indexPath.section][indexPath.row])"
         cell.selectionStyle = .none
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .purple
+
+        let label = UILabel()
+        label.textColor = .white
+        label.accessibilityTraits = .header
+        label.text = "Section \(section)"
+
+        let button = UIButton()
+        button.backgroundColor = .clear
+        button.accessibilityTraits = .button
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Button", for: .normal)
+
+        [label, button].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+        NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            label.trailingAnchor.constraint(greaterThanOrEqualTo: button.leadingAnchor, constant: -16),
+
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            button.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1)
+        ])
+        
+        // Не включать. От этого возникают проблемы со свайпом по заголовкам секций.
+        //view.accessibilityElements = [label, button]
+        return view
     }
 }
